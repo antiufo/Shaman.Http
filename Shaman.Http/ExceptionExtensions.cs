9b721a9f8e6bb7108435d4cc75e5479a0072ed30
Andrea Martinelli <andmartinelli@gmail.com>
2017-03-29 00:10:19 +0200

    ExceptionExtensions.Rethrow

---
+++ Shaman.Http/ExceptionExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+#if !NET35
+using System.Runtime.ExceptionServices;
+#endif
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shaman.Runtime
+{
+    static class ExceptionExtensions
+    {
+
+        public static Exception Rethrow(this Exception ex)
+        {
+#if NET35
+            throw ex;
+#else
+            ExceptionDispatchInfo.Capture(ex).Throw();
+            return ex;
+#endif
+        }
+    }
+}
